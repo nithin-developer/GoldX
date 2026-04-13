12dc41821fc6b9168af01d708625dc723dbca243
Nithin Kumar K <nithinkumar6364@gmail.com>
2026-04-13 14:50:43 +0530

    builded the updated flutter app

--- signalpro/lib/app/config/api_config.dart
+++ signalpro/lib/app/config/api_config.dart
@@ -2,7 +2,7 @@ class ApiConfig {
   // Override with: flutter run --dart-define=API_BASE_URL=http://<host>:8000/api/v1
   static const String baseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    // defaultValue: 'https://api.goldxvip.com/api/v1',
-    defaultValue: 'http://10.45.80.251:8000/api/v1',
+    defaultValue: 'https://api.goldxvip.com/api/v1',
+    // defaultValue: 'http://10.45.80.251:8000/api/v1',
   );
 }
